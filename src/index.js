d58c08db38cc2d0ada79ef1be34cafa348fc0f28
@jhonatanberriosolave <jhonatanberriosolave@gmail.com>
2022-10-16 13:40:12 -0500

    cors

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ import {PORT} from './config.js'
 
 const app  = express()
 
+app.use(express.json())
 app.use(cors())
 
-app.use(express.json())
 
 app.use(rutasPersonajes)
 app.use(rutasVehiculos)
